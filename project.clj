c12555847f912868fc60826af19bec96740d8508
Dimitrios Piliouras <jimpil1985@gmail.com>
2019-06-07 18:15:03 +0100

    0.4.3 released - prepare for next iteration

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject duratom "0.4.3-SNAPSHOT"
+(defproject duratom "0.4.4-SNAPSHOT"
   :description "A durable atom type for Clojure."
   :url "https://github.com/jimpil/duratom"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/java.jdbc "0.6.1"]
+  ;:dependencies []
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
+                                  [org.clojure/java.jdbc "0.6.1"]
                                   [org.postgresql/postgresql "9.4.1208.jre7"] ;; PGSQL driver
                                   [amazonica "0.3.58"]
                                   [com.taoensso/nippy "2.13.0"]]}}
