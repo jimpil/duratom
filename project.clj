ca3e9cc14c08d69b891d2f70eb186719a37016cc
Dimitrios Piliouras <jimpil1985@gmail.com>
2019-09-26 15:43:32 +0100

    prepare for next dev iteration 0.4.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duratom "0.4.5-SNAPSHOT"
+(defproject duratom "0.4.6-SNAPSHOT"
   :description "A durable atom type for Clojure."
   :url "https://github.com/jimpil/duratom"
   :license {:name "Eclipse Public License"
