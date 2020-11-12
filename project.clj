3633f1c709ed18d75491eef6919899ee3dda8939
jimpil <jimpil1985@gmail.com>
2020-11-12 18:16:35 +0000

    Version 0.5.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duratom "0.5.3"
+(defproject duratom "0.5.4-SNAPSHOT"
   :description "A durable atom/agent type for Clojure."
   :url "https://github.com/jimpil/duratom"
   :license {:name "Eclipse Public License"
