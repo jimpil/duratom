d5310057eaaacaf59b2233cdab5c8d9a0341b724
Dimitrios Piliouras <jimpil1985@gmail.com>
2023-04-09 15:27:32 +0100

    Version 0.5.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duratom "0.5.7"
+(defproject duratom "0.5.8-SNAPSHOT"
   :description "A durable atom/agent type for Clojure."
   :url "https://github.com/jimpil/duratom"
   :license {:name "Eclipse Public License"
